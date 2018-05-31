429d84436f1cafc6e4b0c0c4d775855ddbc2d183
alissondiane <diane_10_96@hotmail.com>
2018-05-30 22:02:35 -0500

    creando-TableHeader-NT

---
+++ src/Componentes/Table-Header-NT.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class TableHeaderNT extends React.Component {
+
+  render() {
+    return(
+    <thead>
+			<tr>      
+                <th className="thNT">NIVEL</th>           
+                <th className="thNT">TURNO</th>
+                <th className="thNT">HORA_INICIO</th>
+                <th className="thNT">HORA_FIN</th>
+            </tr>
+	  </thead>
+    )
+  }
+}
+
+export default TableHeaderNT
